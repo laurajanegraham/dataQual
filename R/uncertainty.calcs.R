2c89b43c50ce588bc627f7b4d06516ae514e9ae0
Laura Graham <lgxlg3@nottingham.ac.uk>
2014-07-16 19:19:25 +0100

    Update uncertainty.calcs to be usable on any gridded species data.

--- R/uncertainty.calcs.R
+++ R/uncertainty.calcs.R
@@ -9,34 +9,32 @@
 
 ################################################################################
 
-uncertainty.calc <- function(species.group, grid.file) {
+uncertainty.calcs <- function(species.dat, grid.poly, species.col, grid.col, n.rand=100) {
   require(rgdal)
-  # load species data and select necessary columns
-  species.dat <- get(load(paste0("data/all_",species.group,"_species.rda")))
-  species.dat <- species.dat[,c("grid_ref", "species")]
-  no.species <- length(unique(species.dat$species))
+  # error checking
+  if(!grid.col %in% colnames(species.dat)) stop("Please specify the column containing the grid references")
+  if(!species.col %in% colnames(species.dat)) stop("Please specify the column containing the species names")
+  if(!grid.col %in% names(grid.poly)) stop("The grid reference for the shapefile must match that of the species data")
+  if(class(grid.poly)!="SpatialPolygonsDataFrame") stop("Grid file must be class SpatialPolygonsDataFrame")
   
-  
-  # load shapefiles
-  grid.poly <- readOGR("data/shapes", grid.file, verbose=FALSE)
-  
-  # get rid of spaces and put to lower case to avoid duplicate species names 
-  species.dat$Species <- gsub(" ", ".", tolower(species.dat$species), fixed=T)
+  # select necessary columns
+  species.dat <- species.dat[,c(grid.col, species.col)]
+  no.species <- length(unique(species.dat[,species.col]))
   
   # merge with grids to get only those in study area
   species.dat <- merge(species.dat, grid.poly@data)
   
   # create a list of the grid cells for looping
-  grid.list <- unique(species.dat$grid_ref)
+  grid.list <- unique(species.dat[,grid.col])
   
-  res <- list()
+  cc.list <- list()
   
   for(grid in grid.list) {
-    grid.dat <- species.dat[species.dat$grid_ref==grid,2]
+    grid.dat <- species.dat[species.dat[,grid.col]==grid,species.col] 
     grid.res <- data.frame(NA, nrow=length(grid.dat), ncol=100)
     if(length(grid.dat) >= no.species/2 && length(unique(grid.dat)) > 1) { # only calculate the slope for those with a sampling effort of records >= 2*species
-      for(i in 1:100) {
-        # create 100 randomisations of the records list
+      for(i in 1:n.rand) {
+        # create n randomisations of the records list
         grid.dat <- sample(grid.dat, size=length(grid.dat))
         
         # create number of species at 1, 2, 3, ....j records
@@ -45,32 +43,30 @@ uncertainty.calc <- function(species.group, grid.file) {
         }
       }
       
-      res[[grid]] <- data.frame(records = 1:length(grid.dat), mean.species = rowMeans(grid.res))
+      cc.list[[grid]] <- data.frame(records = 1:length(grid.dat), mean.species = rowMeans(grid.res))
     }
   }
   
-  save(res, file=paste0("results/uncertainty/slopes.", species.group, ".rda"))
-  
   ### create the results for the output table ###
-  uncertainty.results <- data.frame(grid.ref = NA, clench.slope = NA, clench.a = NA, 
+  results <- data.frame(grid.ref = NA, clench.slope = NA, clench.a = NA, 
                                     clench.b = NA, no.records = NA, sp.richness = NA)
   i <- 1
   
-  for(grid in names(res)){
-    clench <- nls(mean.species~(a*records)/(1 + b*records), data=res[[grid]], 
+  for(grid in names(cc.list)){
+    clench <- nls(mean.species~(a*records)/(1 + b*records), data=cc.list[[grid]], 
                   start = list(a=1, b=1))
     
     clench.slope <- function(x) {
       coef(clench)[1]/(coef(clench)[2]^2*x^2 + 2*coef(clench)[2]*x + 1)
     }  
-    uncertainty.results[i, 1] <- grid
-    uncertainty.results[i, 2] <- clench.slope(nrow(res[[grid]]))
-    uncertainty.results[i, 3] <- coef(clench)[1]
-    uncertainty.results[i, 4] <- coef(clench)[2]
-    uncertainty.results[i, 5] <- nrow(res[[grid]])
-    uncertainty.results[i, 6] <- max(res[[grid]][,2])
+    results[i, 1] <- grid
+    results[i, 2] <- clench.slope(nrow(cc.list[[grid]]))
+    results[i, 3] <- coef(clench)[1]
+    results[i, 4] <- coef(clench)[2]
+    results[i, 5] <- nrow(cc.list[[grid]])
+    results[i, 6] <- max(cc.list[[grid]][,2])
     i <- i + 1
   }
-  
-  save(uncertainty.results, file=paste0("results/uncertainty/", species.group, ".uncertainty.rda"))
+  output <- list(results=results, cc.list=cc.list)
+  return(output)
 }
